ffc5ad904c782dd14fdda027d4c30aedde58a434
Pucich <62430530+Pucich@users.noreply.github.com>
2026-02-11 09:15:29 +0300

    Delete test1 directory

--- test1/build-config.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  // Single source of truth for build URLs.
-  // Change only these two fields when switching versions.
-  window.CMP_BUILD_CONFIG = {
-    liteUrl: '/2025.12.13_build_cut_SergeyZ/',
-    fullUrl: 'https://c-m-p.mooo.com/prod_bilds/2025.12.25_new_api_serv/',
-  };
-})();
