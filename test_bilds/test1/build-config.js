55995d1ad2191e4b550cce2295c8bcb872295541
Pucich <62430530+Pucich@users.noreply.github.com>
2026-02-11 09:18:37 +0300

    Add files via upload

---
+++ test_bilds/test1/build-config.js
@@ -0,0 +1,8 @@
+(function () {
+  // Single source of truth for build URLs.
+  // Change only these two fields when switching versions.
+  window.CMP_BUILD_CONFIG = {
+    liteUrl: '/2025.12.13_build_cut_SergeyZ/',
+    fullUrl: '/prod_bilds/2025.12.25_new_api_serv/',
+  };
+})();
